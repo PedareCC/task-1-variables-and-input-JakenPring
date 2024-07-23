e0691dd19979b6168e27a256578bf52bd5d0684e
JakenPring <162660319+JakenPring@users.noreply.github.com>
2024-07-23 05:01:37 +0000

    Mission 2

--- Mission2.py
+++ Mission2.py
@@ -9,19 +9,18 @@ speed_kmph = 1000          # the average speed of a rocket in kilometers per hou
 
 #Step 2: Convert the distance to mars from millions of kilometers to just kilometers (multiply by 1 million). This new variable has been named distance_kim.
 
-distance_km = 
+distance_km = distance_to_mars * 1000000
 
 
 
 #Step 3: Calculate the total travel time in hours by dividing the distance_km by the speed_kmph
 
-travel_time_hours = 
+travel_time_hours = distance_km / speed_kmph
 
 
 
 #Step 4: Convert travel time from hours to days by creating a new variable called 'travel_time_days.'
 
-travel_time_days = 
-
+travel_time_days = travel_time_hours / 24
 
 #Step 5: Check back through your code and make sure that everything that needs to be an integer or a float, is.
